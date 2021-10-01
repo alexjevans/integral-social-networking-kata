a936a060bc13aa9a0a1e9228d4a9ff1f8447cc05
Alexander Evans <alex@evansalex.com>
2021-09-30 19:59:52 -0500

    Added TimeResolver to User and updated publish method to accept the time the post is made.

--- Integral/Integral/User.cs
+++ Integral/Integral/User.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using Integral.Time;
 
 namespace Integral
 {
     public class User
     {
+        public ITimeResolver time { get; private set; }
+
         private List<string> posts;
 
         public User()
         {
             posts = new List<string>();
+            time = new TimeResolver();
         }
 
-        public void Publish(string post)
+        public void Publish(string post, DateTime time = default)
         {
             posts.Add(post);
         }
